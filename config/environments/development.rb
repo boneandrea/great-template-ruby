d14d08d8211f9ed3349708587312e63623057a82
boneandrea <bone.andrea.anthrax@gmail.com>
2023-02-22 01:00:09 +0900

    remove log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.logger = Logger.new('log/mail.log', 'weekly')
 
   config.action_mailer.perform_caching = false
   # Use mailhog
